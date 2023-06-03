1f1a6049bf5eae069bc167e5e376a46971464bcf
Attila fawal <attila_faweli@hotmail.com>
2023-06-03 12:34:29 +0000

    modified  run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import os
 import json
 from flask import Flask, render_template, request, flash
-if os.path.exists("env.py"):
-    import env
 
+# Check if the 'env.py' file exists
+if os.path.exists("env.py"):
+    import env  # If it exists, import it
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
